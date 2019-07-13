6db5181c579f8a3f9454077784b4ea92dec466e0
sam <samlojpur@gmail.com>
2019-07-13 16:49:55 -0400

    error fixing

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -61,7 +61,10 @@ exports.signup = (req, res) => {
       console.error(err);
       if (err.code === "auth/email-already-in-use") {
         return res.status(400).json({ email: "Email is already in use" });
+      } else if (err.code == undefined) {
+        console.log('Didn\'t send error log as json')
       } else {
+        console.log(err.code)
         return res.status(500).json({ error: err.code });
       }
     });
@@ -97,66 +100,6 @@ exports.login = (req, res) => {
       } else return res.status(500).json;
     });
 };
-
-// exports.uploadImage = (req, res) => {
-//   const BusBoy = require("busboy");
-//   const path = require("path");
-//   const os = require("os");
-//   const fs = require("fs");
-
-//   const busboy = new BusBoy({ headers: req.headers });
-
-//   let imageToBeUploaded = {};
-//   let imageFileName;
-
-//   busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
-//     console.log("help");
-//     if (mimetype !== "image/jpeg" && mimetype !== "image/png") {
-//       return res.status(400).json({ error: "Wrong file type submitted" });
-//     }
-
-//     const imageExtension = filename.split(".")[filename.split(".").length - 1];
-//     const randomName = Math.round(Math.random() * 1000000).toString();
-//     imageFileName = `${randomName}.${imageExtension}`;
-//     const filepath = path.join(os.tmpdir(), imageFileName);
-//     imageToBeUploaded = { filepath, mimetype };
-//     console.log(imageToBeUploaded);
-
-//     file.pipe(fs.createWriteStream(filepath));
-//   });
-
-//   busboy.on("finish", () => {
-//     admin
-//       .storage()
-//       .bucket()
-//       .upload(imageToBeUploaded.filepath, {
-//         resumable: false,
-//         metadata: {
-//           metadata: {
-//             contentType: imageToBeUploaded.mimetype
-//           }
-//         }
-//       })
-//       .then(() => {
-//         const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${
-//           config.storageBucket
-//         }/o/${imageFileName}?alt=media`;
-//         return db.doc(`/users/${req.user.handle}`).update({ imageUrl });
-//       })
-//       .then(() => {
-//         return res.json({ message: "Image uploaded successfully" });
-//       })
-//       .catch(err => {
-//         console.error(err);
-//         return res.status(500).json({ error: "something went wrong" });
-//       });
-//   });
-//   busboy.end(req.rawBody);
-//   busboy.on("error", err => {
-//     console.error("error");
-//     console.error(err);
-//   });
-// };
 
 exports.uploadImage = (req, res) => {
   const BusBoy = require("busboy");
--- functions/util/admin.js
+++ functions/util/admin.js
@@ -1,7 +1,10 @@
 const admin = require("firebase-admin");
 
-admin.initializeApp();
+admin.initializeApp({ credential: admin.credential.applicationDefault() });
 
+// const db = admin.firestore();
+
+// const app = admin.initializeApp();
 const db = admin.firestore();
 
 module.exports = { admin, db };
