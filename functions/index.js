9f76083d4cfc06e25d409a278ab01bf55bb00be8
sam <samlojpur@gmail.com>
2019-07-13 16:49:47 -0400

    Linux changes

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require("firebase-functions");
 
 const app = require("express")();
 
-const FBAuth = require("./util/fbAuth");
+const FBAuth = require("./util/FBAuth");
 
 const { getAllScreams, postScream } = require("./handlers/screams");
 const { signup, login, uploadImage } = require("./handlers/users.js");
