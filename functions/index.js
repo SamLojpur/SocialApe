e6c5c24857e7aae893f2659a843341b4e608bb74
sam <samlojpur@gmail.com>
2019-07-13 21:47:27 -0400

    Likes

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,14 @@ const app = require("express")();
 
 const FBAuth = require("./util/FBAuth");
 
-const { getAllScreams, postScream } = require("./handlers/screams");
+const {
+  getAllScreams,
+  postScream,
+  getScream,
+  commentOnScream,
+  likeScream,
+  unlikeScream
+} = require("./handlers/screams");
 const {
   signup,
   login,
@@ -14,7 +21,10 @@ const {
 
 app.get("/screams", getAllScreams);
 app.post("/scream", FBAuth, postScream);
-app.get("/scream/:screamID", getScream);
+app.get("/scream/:screamId", getScream);
+app.post("/scream/:screamId/comment", FBAuth, commentOnScream);
+app.post("/scream/:screamId/like", FBAuth, likeScream);
+app.post("/scream/:screamId/unlike", FBAuth, unlikeScream);
 
 app.post("/signup", signup);
 app.post("/login", login);
